68e909f9cf0cc059d4defcf2525470bfd5caa190
sanjanasivadas <sanjana.velikoth@gmail.com>
2024-01-07 14:26:18 +0530

    save

---
+++ lib/models/note.dart
@@ -0,0 +1,11 @@
+import 'package:isar/isar.dart';
+
+//this line is needed to generate file 
+//then run: dart run build_runner build
+part 'note.g.dart';
+
+@Collection()
+class Note{
+  Id id = Isar.autoIncrement; //starts to create new notes from 0
+  late String text;   //text in the notes
+}

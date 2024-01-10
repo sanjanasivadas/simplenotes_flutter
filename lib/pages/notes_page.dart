177aaddfa8f8c1acbf0a33fcf374bf33af7c489f
sanjanasivadas <sanjana.velikoth@gmail.com>
2024-01-10 08:23:09 +0530

    save

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,5 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simplenotes_flutter/models/note_database.dart';
+
+import '../models/note.dart';
 
 class NotesPage extends StatefulWidget{
   const NotesPage({super.key});
@@ -8,8 +11,119 @@ class NotesPage extends StatefulWidget{
   State<NotesPage> createState() => _NotesPageState();
 }
   class _NotesPageState extends State<NotesPage> {
+   // text controller to access what the user typed
+   final textController = TextEditingController();
+
+   @override
+  void initState() {
+    super.initState();
+    //on app startup, fetch the existing notes
+    readNotes();
+  }
+
+   //create a note
+    void createNote(){
+      showDialog(
+        context: context,
+         builder: (context) => AlertDialog(
+          content: TextField(
+            controller: textController,
+          ),
+          actions: [
+            //create button
+            MaterialButton(
+              onPressed: () {
+                //add to db
+              context.read<NoteDatabase>().addNote(textController.text);
+
+              //clear controller
+              textController.clear();
+
+              //pop dialog box
+              Navigator.pop(context);
+            },
+            child: const Text("Create"),
+            )
+          ],
+         ),
+      );
+    }
+   //read notes
+   void readNotes(){
+    context.read<NoteDatabase>().fetchNotes();
+   }
+   //update a note
+  void updateNote(Note note){
+    //pre fill the current note text 
+    textController.text = note.text;
+    showDialog(
+      context: context,
+       builder: (context) => AlertDialog(
+        title: Text("Update Note"),
+        content: TextField(controller: textController,),
+        actions: [
+          //update button
+          MaterialButton(onPressed: () {
+            //update note in db
+            context
+            .read<NoteDatabase>()
+            .updateNote(note.id, textController.text);
+            //clear controller
+            textController.clear();
+            //pop dialog box
+            Navigator.pop(context);
+          },
+          child: const Text("Update"),
+          )
+         ],
+        ),
+      );
+    }
+
+   //delete a note
+  void deleteNote(int id){
+   context.read<NoteDatabase>().deleteNote(id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    // note database
+    final noteDatabase = context.watch<NoteDatabase>();
+
+    //current notes
+    List<Note> currentNotes = noteDatabase.currentNotes;
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: const Icon(Icons.add),
+        ),
+        body: ListView.builder(
+          itemCount: currentNotes.length,
+          itemBuilder: (context, index) {
+            //get individual note
+           final note = currentNotes[index];
+
+           //list tile UI
+           return ListTile(
+            title: Text(note.text),
+            trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //edit button
+              IconButton(onPressed: () => updateNote(note),
+               icon: const Icon(Icons.edit),
+               ),
+
+               //delete button
+               IconButton(onPressed: () => deleteNote(note.id),
+                icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
+          );
+        },
+      )
+    );
     }
   }
